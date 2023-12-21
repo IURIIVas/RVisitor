08b177794256539f613ace238180b4bd4aaa2605
iurii vasilkov <1jura.vas@gmail.com>
2023-12-21 16:36:53 +0300

    spi flash driver init

--- lib/w25q32/w25q32.c
+++ lib/w25q32/w25q32.c
@@ -21,6 +21,145 @@
 
 //------------------------------------------------- Static Functions ---------------------------------------------------
 
+
+
 //---------------------------------------------------- Functions -------------------------------------------------------
 
+uint8_t spif_init(spif_s *spif)
+{
 
+}
+//
+//void spi_flash_init(spi_s *spi)
+//{
+//    spi_init_s spi_init_struct = {0};
+//
+//    spi_init_struct.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
+//    spi_init_struct.SPI_Mode = SPI_Mode_Master;
+//    spi_init_struct.SPI_DataSize = SPI_DataSize_8b;
+//    spi_init_struct.SPI_CPOL = SPI_CPOL_High;
+//    spi_init_struct.SPI_CPHA = SPI_CPHA_2Edge;
+//    spi_init_struct.SPI_NSS = SPI_NSS_Soft;
+//    spi_init_struct.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_4;
+//    spi_init_struct.SPI_FirstBit = SPI_FirstBit_MSB;
+//    spi_init_struct.SPI_CRCPolynomial = 7;
+//    SPI_Init(spi, &spi_init_struct);
+//
+//    SPI_Cmd(spi, ENABLE);
+//}
+//
+//void spi_flash_write_enable(spi_s *spi, gpio_s *cs_gpio, uint8_t cs_port)
+//{
+//    GPIO_WriteBit(cs_gpio, cs_port, 0);
+//    spi_readwrite_byte(spi, w25_write_enable_cmd);
+//    GPIO_WriteBit(cs_gpio, cs_port, 1);
+//}
+//
+//void spi_flash_write_disable(spi_s *spi, gpio_s *cs_gpio, uint8_t cs_port)
+//{
+//    GPIO_WriteBit(cs_gpio, cs_port, 0);
+//    spi_readwrite_byte(spi, w25_write_disable_cmd);
+//    GPIO_WriteBit(cs_gpio, cs_port, 1);
+//}
+//
+//uint16_t spi_flash_read_id(spi_s *spi, gpio_s *cs_gpio, uint8_t cs_port)
+//{
+//    uint16_t byte = 0;
+//
+//    GPIO_WriteBit(cs_gpio, cs_port, 0);
+//    spi_readwrite_byte(spi, SPIF_MANUFACTUR_ID);
+//    spi_readwrite_byte(spi, 0x00);
+//    spi_readwrite_byte(spi, 0x00);
+//    spi_readwrite_byte(spi, 0x00);
+//    byte |= spi_readwrite_byte(spi, 0xFF) << 8;
+//    byte |= spi_readwrite_byte(spi, 0xFF);
+//    GPIO_WriteBit(cs_gpio, cs_port, 1);
+//
+//    return byte;
+//}
+//
+///// \brief read w25q status register
+/////        bit7  6   5   4   3   2   1   0
+/////        spr   rv  tb  bp2 bp1 bp0 wel busy
+///// \param spi
+///// \retval uint8_t
+///// \return status register
+//uint8_t spi_flash_read_sr(spi_s *spi, gpio_s *cs_gpio, uint8_t cs_port)
+//{
+//    uint8_t sr = 0;
+//
+//    GPIO_WriteBit(cs_gpio, cs_port, 0);
+//    spi_readwrite_byte(spi, w25_read_status_reg_cmd);
+//    sr = spi_readwrite_byte(spi, 0xFF);
+//    GPIO_WriteBit(cs_gpio, cs_port, 1);
+//
+//    return sr;
+//}
+//
+//uint8_t spi_flash_wait_busy(spi_s *spi, gpio_s *cs_gpio, uint8_t cs_port)
+//{
+//    uint8_t timeout = 200;
+//
+//    while ((spi_flash_read_sr(spi, cs_gpio, cs_port) & (1 << SR_BUSY_BIT)) && timeout)
+//    {
+//        timeout--;
+//    }
+//
+//    return timeout;
+//}
+//
+//uint8_t spi_flash_erase_sector(spi_s *spi, gpio_s *cs_gpio, uint8_t cs_port, uint32_t sect_numb)
+//{
+//    uint32_t addr = sect_numb * SECTOR_SIZE_BYTES;
+//    uint8_t status = 0;
+//
+//    spi_flash_write_enable(spi, cs_gpio, cs_port);
+//    status = spi_flash_wait_busy(spi, cs_gpio, cs_port);
+//    if (status == FAIL)
+//    {
+//        return status;
+//    }
+//
+//    GPIO_WriteBit(cs_gpio, cs_port, 0);
+//    spi_readwrite_byte(spi, w25_erase_sector_cmd);
+//    spi_readwrite_byte(spi, (uint8_t) ((addr >> 16) && 0xFF));
+//    spi_readwrite_byte(spi, (uint8_t) ((addr >> 8) && 0xFF));
+//    spi_readwrite_byte(spi, (uint8_t) ((addr >> 0) && 0xFF));
+//    GPIO_WriteBit(cs_gpio, cs_port, 1);
+//    status = spi_flash_wait_busy(spi, cs_gpio, cs_port);
+//    if (status == FAIL)
+//    {
+//        return status;
+//    }
+//}
+//
+//void spi_flash_read(spi_s *spi, gpio_s *cs_gpio, uint8_t cs_port,  uint8_t *read_buffer, uint32_t read_addr, uint32_t size)
+//{
+//    GPIO_WriteBit(cs_gpio, cs_port, 0);
+//    spi_readwrite_byte(spi, w25_read_data_cmd);
+//    spi_readwrite_byte(spi, (uint8_t) ((read_addr) >> 16) & 0xFF);
+//    spi_readwrite_byte(spi, (uint8_t) ((read_addr) >> 8) & 0xFF);
+//    spi_readwrite_byte(spi, (uint8_t) ((read_addr) >> 0) & 0xFF);
+//
+//    for (uint32_t idx = 0; idx < size; idx++)
+//    {
+//        read_buffer[idx] = spi_readwrite_byte(spi, 0xFF);
+//    }
+//
+//    GPIO_WriteBit(cs_gpio, cs_port, 1);
+//}
+//
+//void spi_flash_write(spi_s *spi, gpio_s *cs_gpio, uint8_t cs_port,  uint8_t *write_buffer, uint32_t write_addr, uint32_t size)
+//{
+//    uint32_t sector_pos = 0;
+//    uint32_t sector_remain = 0;
+//
+//    sector_pos = write_addr / SECTOR_SIZE_BYTES;
+//    sector_remain = SECTOR_SIZE_BYTES - (write_addr % SECTOR_SIZE_BYTES);
+//
+//    if (size <= sector_remain)
+//    {
+//        sector_remain = size;
+//    }
+//
+//}
--- lib/w25q32/w25q32.h
+++ lib/w25q32/w25q32.h
@@ -13,15 +13,94 @@
 //----------------------------------------------------- Includes -------------------------------------------------------
 
 #include "global_inc.h"
+#include "spi.h"
 
 //------------------------------------------------------ Macros --------------------------------------------------------
 
+#define SPIF_MANUFACTUR_ID_CMD                    (0x90)
+#define w25_write_enable_cmd                      (0x06)
+#define w25_write_disable_cmd                     (0x04)
+#define w25_read_status_reg_cmd                   (0x05)
+#define w25_erase_sector_cmd                      (0x20)
+#define w25_read_data_cmd                         (0x03)
+
+#define SR_BUSY_BIT                               (0)
+
+#define SECTOR_SIZE_BYTES                         (4096)
+#define PAGE_SIZE_BYTES                           (256)
+
 //----------------------------------------------------- Typedefs -------------------------------------------------------
+
+typedef enum
+{
+    SPIF_MANUFACTOR_ERROR = 0,
+    SPIF_MANUFACTOR_WINBOND = 0xEF,
+    SPIF_MANUFACTOR_ISSI = 0xD5,
+    SPIF_MANUFACTOR_MICRON = 0x20,
+    SPIF_MANUFACTOR_GIGADEVICE = 0xC8,
+    SPIF_MANUFACTOR_MACRONIX = 0xC2,
+    SPIF_MANUFACTOR_SPANSION = 0x01,
+    SPIF_MANUFACTOR_AMIC = 0x37,
+    SPIF_MANUFACTOR_SST = 0xBF,
+    SPIF_MANUFACTOR_HYUNDAI = 0xAD,
+    SPIF_MANUFACTOR_ATMEL = 0x1F,
+    SPIF_MANUFACTOR_FUDAN = 0xA1,
+    SPIF_MANUFACTOR_ESMT = 0x8C,
+    SPIF_MANUFACTOR_INTEL = 0x89,
+    SPIF_MANUFACTOR_SANYO = 0x62,
+    SPIF_MANUFACTOR_FUJITSU = 0x04,
+    SPIF_MANUFACTOR_EON = 0x1C,
+    SPIF_MANUFACTOR_PUYA = 0x85,
+} spif_manufactor_e;
+
+
+typedef enum
+{
+    SPIF_SIZE_ERROR = 0,
+    SPIF_SIZE_1MBIT = 0x11,
+    SPIF_SIZE_2MBIT = 0x12,
+    SPIF_SIZE_4MBIT = 0x13,
+    SPIF_SIZE_8MBIT = 0x14,
+    SPIF_SIZE_16MBIT = 0x15,
+    SPIF_SIZE_32MBIT = 0x16,
+    SPIF_SIZE_64MBIT = 0x17,
+    SPIF_SIZE_128MBIT = 0x18,
+    SPIF_SIZE_256MBIT = 0x19,
+    SPIF_SIZE_512MBIT = 0x20,
+} spif_size_e;
+
+typedef struct
+{
+    spi_s *spi;
+    gpio_s *cs_gpio;
+    spif_manufactor_e manufactor;
+    spif_size_e size;
+    uint8_t cs_port;
+    uint8_t lock;
+    uint16_t reserved;
+    uint32_t page_cnt;
+    uint32_t sector_cnt;
+    uint32_t block_cnt;
+} spif_s;
 
 //---------------------------------------------------- Variables -------------------------------------------------------
 
 //------------------------------------------------ Function prototypes -------------------------------------------------
 
+uint8_t spif_init(spif_s *spif);
 
+uint8_t spif_erase_chip(spif_s *spif);
+uint8_t spif_erase_sector(spif_s *spif, uint32_t sect_num);
+uint8_t spif_erase_block(spif_s *spif, uint32_t sect_num);
+
+uint8_t spif_write_addr(spif_s *spif, uint32_t addr, uint8_t *data, uint32_t size);
+uint8_t spif_write_page(spif_s *spif, uint32_t page_num, uint8_t *data, uint32_t size, uint32_t offset);
+uint8_t spif_write_sector(spif_s *spif, uint32_t sector_num, uint8_t *data, uint32_t size, uint32_t offset);
+uint8_t spif_write_block(spif_s *spif, uint32_t block_num, uint8_t *data, uint32_t size, uint32_t offset);
+
+uint8_t spif_read_addr(spif_s *spif, uint32_t addr, uint8_t *data, uint32_t size);
+uint8_t spif_read_page(spif_s *spif, uint32_t page_num, uint8_t *data, uint32_t size, uint32_t offset);
+uint8_t spif_read_sector(spif_s *spif, uint32_t sector_num, uint8_t *data, uint32_t size, uint32_t offset);
+uint8_t spif_read_block(spif_s *spif, uint32_t block_num, uint8_t *data, uint32_t size, uint32_t offset);
 
 #endif /* W25Q32_H_ */
